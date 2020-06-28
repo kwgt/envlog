161dfa1bca7a1e1e8ddcd759805b2870d1668130
Hiroshi Kuwagata <kgt9221@gmail.com>
2020-06-28 20:43:20 +0900

    Bug fix.

--- lib/envlog/logger/input/udp.rb
+++ lib/envlog/logger/input/udp.rb
@@ -12,20 +12,20 @@ require 'socket'
 module EnvLog
   module Logger
     module InputSource
-			SUPPORT_FORMAT_VERSION = 2
+      SUPPORT_FORMAT_VERSION = 2
 
-			JSON_FORMAT = <<~EOT.gsub(/\s/, "")
-				{
-					"addr":"%02x:%02x:%02x:%02x:%02x:%02x",
-					"seq":%d,
-					"temp":%4.1f,
-					"hum":%4.1f,
-					"a/p":%d,
-					"rssi":null,
-					"vbat":%4.2f,
-					"vbus":%4.2f
-				}
-			EOT
+      JSON_FORMAT = <<~EOT.gsub(/\s/, "")
+        {
+          "addr":"%02x:%02x:%02x:%02x:%02x:%02x",
+          "seq":%d,
+          "temp":%4.1f,
+          "hum":%4.1f,
+          "a/p":%d,
+          "rssi":null,
+          "vbat":%4.2f,
+          "vbus":%4.2f
+        }
+      EOT
 
       class << self
         def add_udp_source(src)
@@ -39,18 +39,18 @@ module EnvLog
               dat = sock.recv(1024)
               tmp = dat.unpack("C6CCs<S<S<S<S<")
 
-							next if tmp[6] != SUPPORT_FORMAT_VERSION
+              next if tmp[6] != SUPPORT_FORMAT_VERSION
 
               json = JSON_FORMAT %
-											[tmp[0], tmp[1], tmp[2], tmp[3], tmp[4], tmp[5],
-											 tmp[7],
-											 tmp[8]  / 100.0,
-											 tmp[9]  / 100.0,
-											 tmp[10] / 100.0,
-											 tmp[11] / 100.0,
-											 tmp[12] / 100.0]
+                      [tmp[0], tmp[1], tmp[2], tmp[3], tmp[4], tmp[5],
+                       tmp[7],
+                       tmp[8]  / 100.0,
+                       tmp[9]  / 100.0,
+                       tmp[10] / 10.0,
+                       tmp[11] / 100.0,
+                       tmp[12] / 100.0]
 
-							put_data(json)
+              put_data(json)
 
             rescue Exit
               break
