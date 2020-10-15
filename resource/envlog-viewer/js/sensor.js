979db5433ed1f36644b18f9ed83bdf1ffdef2ecf
Hiroshi Kuwagata <kgt9221@gmail.com>
2020-10-15 21:43:23 +0900

    navbarを設置し、センサーの設置場所の表示を行うようにした。

--- resource/envlog-viewer/js/sensor.js
+++ resource/envlog-viewer/js/sensor.js
@@ -1197,7 +1197,12 @@
       .then((config) => {
         graphConfig = config;
 
-        return session.getLatestSensorValue(sensorId)
+        return session.getSensorInfo(sensorId);
+      })
+      .then((info) => {
+        $('nav > h4').text(info["descr"]);
+
+        return session.getLatestSensorValue(sensorId);
       })
       .then((info) => {
         setSensorValue(info);
